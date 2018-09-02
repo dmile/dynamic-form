0daf988ef3af6ebc3e86792b74931144540878a2
dmitry <lepsky.dmitry@gmail.com>
2018-09-02 21:15:48 +0300

    bootstrap switched to sass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import AppContainer from './containers/AppContainer';
 
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-theme.css'
+import './index.css';
 
 ReactDOM.render(
     <AppContainer/>,
